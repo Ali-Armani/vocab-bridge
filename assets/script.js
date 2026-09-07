4b2b43022df4563bb34c18b8006cf1ddf33942ac
Ali Armani <armani.ali@proton.me>
2026-09-07 17:17:59 +0330

    feat: Automatically set the footer copyright year

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,5 @@
-
+// Automatically set the footer copyright year
+const footerYear = document.getElementById("footer-year");
+if (footerYear) {
+    footerYear.textContent = new Date().getFullYear();
+}
